333ca1e0149bfb40b39953d676884b9678790b31
bulutcan99 <gocerbulutcan@gmail.com>
2024-06-30 23:40:54 +0300

    feat: added response and error

--- src/application/common/api_response.rs
+++ src/application/common/api_response.rs
@@ -1,6 +1,13 @@
+use axum::{
+	http::StatusCode,
+	Json
+	,
+	response::IntoResponse,
+};
+use axum::response::Response;
 use serde::Serialize;
-use axum::response::
-use super::util::date_time::DateService;
+
+use crate::application::common::util::date_time::DateService;
 
 #[derive(Debug, Serialize)]
 pub struct ApiResponse<T>
@@ -8,6 +15,7 @@ where
 	T: Serialize,
 {
 	code: u16,
+	internal_code: Option<u32>,
 	error: bool,
 	message: Option<String>,
 	result: Option<T>,
@@ -21,30 +29,45 @@ where
 	pub fn success(message: Option<String>, result: Option<T>) -> Self {
 		Self {
 			code: 200,
+			internal_code: None,
 			error: false,
 			message,
 			result,
-			timestamp: DateService::get_cuurent_timestamp(),
-        }
+			timestamp: DateService::current_timestamp(),
+		}
 	}
 
 	pub fn error(code: u16, message: Option<String>, result: Option<T>) -> Self {
 		Self {
 			code,
+			internal_code: None,
 			error: true,
 			message,
 			result,
-			timestamp: DateService::get_cuurent_timestamp(),
-        }
+			timestamp: DateService::current_timestamp(),
+		}
 	}
 }
 
-//TODO: seprate error codes with http status code
-impl<T> Into<HttpResponse> for ApiResponse<T>
+impl<T> IntoResponse for ApiResponse<T>
 where
 	T: Serialize,
 {
-	fn into(self) -> HttpResponse {
-		HttpResponse::build(StatusCode::from_u16(self.code).unwrap()).json(self)
+	fn into_response(self) -> Response {
+		let status = StatusCode::from_u16(self.code).unwrap_or(StatusCode::INTERNAL_SERVER_ERROR);
+		(status, Json(self)).into_response()
 	}
 }
+// //Example:
+// async fn hello_handler() -> impl IntoResponse {
+// 	ApiResponse::success(Some("Hello, world!".to_string()), Some("This is the result".to_string()))
+// }
+//
+// async fn error_handler() -> impl IntoResponse {
+// 	ApiResponse::error(
+// 		404,
+// 		Some(1001),
+// 		Some("Resource not found".to_string()),
+// 		None::<String>,
+// 	)
+// }
--- src/application/common/error.rs
+++ src/application/common/error.rs
@@ -1,118 +1,106 @@
 use std::vec;
 
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
-use serde::{Serialize};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::Serialize;
 use thiserror::Error as ThisError;
 use validator::{ValidationErrors, ValidationErrorsKind::Field};
-use sqlx::Error as SqlxError;
 
-use super::api_response::ApiResponse;
+use crate::application::common::api_response::ApiResponse;
 
 #[derive(Debug, ThisError)]
 pub enum Error {
-    #[error("Oops! Something went wrong.")]
-    InternalServerError,
-    #[error("{0}")]
-    EntityNotFound(String),
-    #[error("{0}")]
-    EntityExists(String),
-    #[error("{0}")]
-    EntityValidationFailed(String),
-    #[error("Input validation failed.")]
-    InputValidation(Vec<FieldError>),
-    #[error("{0}")]
-    AuthorizationFailed(String),
-    #[error("Database operation failed")]
-    DatabaseFailed
+	#[error("Oops! Something went wrong.")]
+	InternalServerError,
+	#[error("{0}")]
+	EntityNotFound(String),
+	#[error("{0}")]
+	EntityExists(String),
+	#[error("{0}")]
+	EntityValidationFailed(String),
+	#[error("Input validation failed.")]
+	InputValidation(Vec<FieldError>),
+	#[error("{0}")]
+	AuthorizationFailed(String),
+	#[error("Database operation failed")]
+	DatabaseFailed,
 }
 
-impl ResponseError for Error {
-    fn status_code(&self) -> StatusCode {
-        match self {
-            Self::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
-            Self::EntityNotFound(_) => StatusCode::NOT_FOUND,
-            Self::EntityExists(_) => StatusCode::CONFLICT,
-            Self::InputValidation(_) => StatusCode::CONFLICT,
-            Self::EntityValidationFailed(_) => StatusCode::CONFLICT,
-            Self::AuthorizationFailed(_) => StatusCode::UNAUTHORIZED,
-            Self::DatabaseFailed => StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+impl IntoResponse for Error {
+	fn into_response(self) -> Response {
+		let status_code = self.status_code();
+		let message = self.to_string();
 
-    fn error_response(&self) -> HttpResponse {
-        let status_code = self.status_code();
-        let message = self.to_string();
+		match self {
+			Self::InputValidation(fields) => {
+				let result = fields;
+				ApiResponse::error(status_code.as_u16(), Some(message), Some(result)).into_response()
+			}
+			_ => {
+				ApiResponse::<()>::error(status_code.as_u16(), Some(message), None).into_response()
+			}
+		}
+	}
+}
 
-        match self {
-            Self::InputValidation(fields) => {
-                let result = fields.clone();
-
-                ApiResponse::error(
-                    status_code.as_u16(),
-                    Some(message),
-                    Some(result)
-                ).into()
-            }
-            _ => {
-                ApiResponse::<()>::error(
-                    status_code.as_u16(),
-                    Some(message),
-                    None
-                ).into()
-            }
-        }
-    }
+impl Error {
+	fn status_code(&self) -> StatusCode {
+		match self {
+			Self::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::EntityNotFound(_) => StatusCode::NOT_FOUND,
+			Self::EntityExists(_) => StatusCode::CONFLICT,
+			Self::InputValidation(_) => StatusCode::BAD_REQUEST,
+			Self::EntityValidationFailed(_) => StatusCode::BAD_REQUEST,
+			Self::AuthorizationFailed(_) => StatusCode::UNAUTHORIZED,
+			Self::DatabaseFailed => StatusCode::INTERNAL_SERVER_ERROR,
+		}
+	}
 }
 
 impl From<ValidationErrors> for Error {
-    fn from(value: ValidationErrors) -> Self {
-        let mut fields: Vec<FieldError> = vec![];
+	fn from(value: ValidationErrors) -> Self {
+		let mut fields: Vec<FieldError> = vec![];
 
-        for (field_name, error) in value.errors().iter() {
-            if let Field(err) = error {
-                let field_error = FieldError {
-                    name: field_name.to_string(),
-                    reason: err
-                        .iter()
-                        .next()
-                        .unwrap()
-                        .message
-                        .as_ref()
-                        .unwrap()
-                        .to_string(),
-                };
+		for (field_name, error) in value.errors().iter() {
+			if let Field(err) = error {
+				let field_error = FieldError {
+					name: field_name.to_string(),
+					reason: err
+						.iter()
+						.next()
+						.unwrap()
+						.message
+						.as_ref()
+						.unwrap()
+						.to_string(),
+				};
 
-                fields.push(field_error);
-            }
-        }
+				fields.push(field_error);
+			}
+		}
 
-        Self::InputValidation(fields)
-    }
-}
-
-impl From<SqlxError> for Error {
-    fn from(_: SqlxError) -> Self {
-        Self::DatabaseFailed
-    }
+		Self::InputValidation(fields)
+	}
 }
 
 #[derive(Debug, Serialize)]
 struct CommonErrorResponse {
-    code: u16,
-    message: String,
-    timestamp: u64,
+	code: u16,
+	message: String,
+	timestamp: u64,
 }
 
 #[derive(Debug, Serialize)]
 struct ValidationErrorResponse {
-    code: u16,
-    message: String,
-    fields: Vec<FieldError>,
-    timestamp: u64,
+	code: u16,
+	message: String,
+	fields: Vec<FieldError>,
+	timestamp: u64,
 }
+
 
 #[derive(Debug, Clone, Serialize)]
 pub struct FieldError {
-    name: String,
-    reason: String,
+	name: String,
+	reason: String,
 }
